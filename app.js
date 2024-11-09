bb0dad10a551635fb2d22905cdb205ac817ee870
ayousef27 <ahmenero27@gmail.com>
2024-11-09 10:51:18 +0300

    finished

--- app.js
+++ app.js
@@ -16,12 +16,99 @@ const game = {
     { name: 'rare candy', quantity: 99 }
   ]
 }
-console.dir(pokemon, { maxArrayLength: null })
-console.log(pokemon[59].name)
-console.log(game)
-
+//console.dir(pokemon, { maxArrayLength: null })
+console.log(pokemon[58].name)
+//console.log(game)
 game.difficulty = 'Med'
+const starterPokemon = pokemon.find((pokemon) => pokemon.starter === true)
+game.party.push(starterPokemon)
+const charmander = pokemon.find((pokemon) => pokemon.number === 4)
+const pidgey = pokemon.find((pokemon) => pokemon.number === 16)
+const squirtle = pokemon.find((pokemon) => pokemon.number === 7)
+game.party.push(charmander, pidgey, squirtle)
+//console.log(game)
+game.gyms.forEach((gym) => {
+  if (gym.difficulty < 3) {
+    game.completed = true
+  }
+})
+//console.log(game)
 
-let pokst = pokemon[0]
-game.party.push(pokst)
+game.party.splice(0, 1, pokemon[1])
+game.party.splice(1, 1, pokemon[4])
+game.party.splice(2, 1, pokemon[7])
+game.party.splice(3, 1, pokemon[25])
 console.log(game.party)
+
+game.party.forEach((pokemon) => {
+  //console.log(pokemon.name)
+})
+
+for (let i = 0; i < pokemon.length; i++) {
+  if (pokemon[i].starter === true) {
+    console.log(pokemon[i].name)
+  }
+}
+game.catchPokemon = function (pokemonObj) {
+  this.party.push(pokemonObj)
+}
+
+game.catchPokemon(pokemon[6])
+
+console.log(game.party)
+
+function catchPokemon(pokemonObj) {
+  game.party.push(pokemonObj)
+
+  const pokeballItem = game.items.find((item) => item.name === 'pokeball')
+
+  if (pokeballItem) {
+    pokeballItem.quantity -= 1
+  }
+}
+
+catchPokemon(pokemon[0])
+
+console.log(game.items)
+
+game.gyms.forEach((gym) => {
+  if (gym.difficulty < 6) {
+    gym.completed = true
+  }
+})
+console.log(game.gyms)
+
+game.gymStatus = function () {
+  const gymTally = {
+    completed: 0,
+    incomplete: 0
+  }
+
+  game.gyms.forEach((gym) => {
+    if (gym.completed) {
+      gymTally.completed += 1
+    } else {
+      gymTally.incomplete += 1
+    }
+  })
+
+  console.log(gymTally)
+}
+
+game.gymStatus()
+
+game.partyCount = function () {
+  return game.party.length
+}
+
+console.log(game.partyCount())
+
+game.gyms.forEach((gym) => {
+  if (gym.difficulty < 8) {
+    gym.completed = true
+  }
+})
+
+console.log(game.gyms)
+
+console.log(game)
